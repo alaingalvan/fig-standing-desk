3e7d76fc7d7ed1dfb1031cedb343bac7739e5faf
Alain Galvan <hi@alain.xyz>
2017-01-11 22:08:35 -0500

    Update lib.rs

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,6 +1,16 @@
 #[cfg(test)]
 mod tests {
+
+    /// Sends a valid request to the API and checks if it responds correctly
     #[test]
-    fn it_works() {
+    fn http_send_success() {
+         assert!(false);
     }
+
+    /// Sends an invalid request and checks if we recieve a 400 error.
+    #[test]
+    fn http_send_error() {
+        assert!(false);
+    }
+
 }
