95c39eda6ae42ef2bf9f56f1c001c648df6c46b0
Alain Galvan <hi@alain.xyz>
2017-01-13 15:28:44 -0500

    Fixed API bugs

--- server/src/http/mod.rs
+++ server/src/http/mod.rs
@@ -10,21 +10,19 @@ struct APIRequest {
 
 /// Creates a Nickel HTTP Server
 pub fn create_server() -> Nickel {
+
     let mut server = Nickel::new();
+
     let mut router = Nickel::router();
 
-    router.post("/api", middleware! { |request, response |
+    router.post("/api", middleware! { |request, response|
 
       let data = request.json_as::<APIRequest>().unwrap();
 
-      // Check data validity
-
       // Perform request with gpio device.
       gpio::send(data.vector, data.time);
-      
-      // Set the returned type as JSON
-      response.set(MediaType::Json);
-      response.send(format!("error"));
+
+      format!("{{}}")
 
     });
 
