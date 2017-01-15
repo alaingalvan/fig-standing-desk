6ff7fe366db781831e7e8682e800124b7c1b5386
Alain Galvan <hi@alain.xyz>
2017-01-14 19:46:32 -0500

    Attempt at memory mapped magic

--- server/src/http/gpio.rs
+++ server/src/http/gpio.rs
@@ -1,18 +1,44 @@
-use sysfs_gpio;
-use sysfs_gpio::{Direction, Pin};
-
 use std::thread::sleep;
 use std::time::Duration;
 
-pub fn send(dir: f32, time: u32) {
-    println!("Recived request to move {} direction for {} milliseconds.", dir, time);
-    // Select pins from pin map
-    //let (pwm1, dir1, pwm2, dir2) = (Pin::new(18), Pin::new(17), Pin::new(14), Pin::new(15));
-    //let mut v = vec![pwm1, dir1, pwm2, dir2];
+pub fn send(vector: f32, time: u32) {
+    println!("Recived request to move {} direction for {} milliseconds.", vector, time);
 
-    //pwm1.set_direction(Direction::Out);
+    // Convert direction to normalized float and bool direction.
+    // ================================
 
-  
+    let length = vector.abs().rem(1.0);
+
+    let direction = vector > 0.0;
+
+    println!("Going in dir {} at length {}.", direction, length);
+
+    // Write Direction to GPIO 6 and 17
+    // Refer to BCM2835 ARM Peripherals page 89
+    // ================================
+
+
+
+    // Write to PWM Registers
+    // Refer to BCM2835 ARM Peripherals page 138
+    // ================================
+
+    // CTL Register
+    let ctl_mask: u32 = 0b1000000110000001;
+
+    // DMAC register
+    let dmac_mask: u32 = 0x8000;
+
+    // RNG1 register (Pulse Width)
+    let rng1_mask: u32 = 32 * length;
+
+    // DTA1 register (Pulses)
+    let data1_mask: u32 = 0x1;
+
+    // GPIO12 ALT0
+
+    // GPIO18 ALT5
+
 }
 
 pub fn poll(pin_num: u64) -> sysfs_gpio::Result<()> {
