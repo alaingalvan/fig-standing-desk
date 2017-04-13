6cfc5818a137ee9141804a7f15fb05da9ef80819
Alain Galvan <hi@alain.xyz>
2017-04-13 19:40:00 -0400

    Updated distribution, server

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,21 +2,22 @@
 extern crate nickel;
 extern crate rustc_serialize;
 extern crate sysfs_gpio;
-extern crate sysfs_pwm;
 
 mod http;
 
+static SERVER_MESSAGE: &'static str = "\
+🍐 Fig Standing Desk Server
+Version 1.4.2 (April 13, 2017)
+================================";
+
 fn main() {
 
-    println!("
-🍐 Fig Standing Desk Server
-Version 1.4.1 (Feburary 26, 2017)
-================================");
+    println!("{}", SERVER_MESSAGE);
 
     let server = http::create_server();
     
     server.listen("localhost:3007")
         .expect("Failed to launch server!");
 
-    println!("Listening @ port 3007");
+    println!("Listening @ port 3007, check the /api endpoint!");
 }
